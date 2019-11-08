2680b1236b20b938b09087e6218990d024b7e57f
alperaltuntas <alperaltuntas@gmail.com>
2019-11-07 17:21:17 -0700

    initialize io_pio module

---
+++ mpp/mpp_io_pio.F90
@@ -0,0 +1,64 @@
+!***********************************************************************
+!*                   GNU Lesser General Public License
+!*
+!* This file is part of the GFDL Flexible Modeling System (FMS).
+!*
+!* FMS is free software: you can redistribute it and/or modify it under
+!* the terms of the GNU Lesser General Public License as published by
+!* the Free Software Foundation, either version 3 of the License, or (at
+!* your option) any later version.
+!*
+!* FMS is distributed in the hope that it will be useful, but WITHOUT
+!* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+!* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+!* for more details.
+!*
+!* You should have received a copy of the GNU Lesser General Public
+!* License along with FMS.  If not, see <http://www.gnu.org/licenses/>.
+!***********************************************************************
+!-----------------------------------------------------------------------
+!                 ParallelIO (PIO) Module for FMS
+!
+! AUTHOR: A. Altuntas (altuntas@ucar.edu)
+!         National Center for Atmospheric Research
+!
+! This program is free software; you can redistribute it and/or modify
+! it under the terms of the GNU General Public License as published by
+! the Free Software Foundation; either version 2 of the License, or
+! (at your option) any later version.
+!
+! This program is distributed in the hope that it will be useful,
+! but WITHOUT ANY WARRANTY; without even the implied warranty of
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+! GNU General Public License for more details.
+!
+! For the full text of the GNU General Public License,
+! write to: Free Software Foundation, Inc.,
+!           675 Mass Ave, Cambridge, MA 02139, USA.
+!-----------------------------------------------------------------------
+
+! <CONTACT EMAIL="altuntas@ucar.edu">
+!   A. Altuntas
+! </CONTACT>
+
+! <OVERVIEW>
+!   <TT>mpp_io_pio_mod</TT>, is ... TODO
+! </OVERVIEW>
+
+! <DESCRIPTION>
+!   TODO
+! </DESCRIPTION>
+
+module mpp_io_pio_mod
+
+#include <fms_platform.h>
+#define _MAX_FILE_UNITS 1024
+
+!use pio, only : TODO
+
+implicit none
+private
+
+integer :: TODO
+
+end module mpp_io_pio_mod
