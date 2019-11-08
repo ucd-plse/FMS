92cfc82e78912f8c5e819ef0b9d1270f1c814c9c
alperaltuntas <alperaltuntas@gmail.com>
2019-11-08 14:54:48 -0700

    add a call to PIO_init

--- mpp/mpp_pio.F90
+++ mpp/mpp_pio.F90
@@ -50,28 +50,43 @@
 ! </DESCRIPTION>
 
 module mpp_pio_mod
+
+! PIO only works with MPI:
+#ifndef use_libMPI
+#undef use_PIO
+#endif
+
 #ifdef use_PIO
 
 #include <fms_platform.h>
 
-  use mpp_mod,         only : input_nml_file
-  use pio, only : iosystem_desc_t
+  use mpp_mod,  only : input_nml_file
+  use mpp_mod,  only : mpp_pe, mpp_root_pe, mpp_npes, get_mpp_comm
+  use pio,      only : PIO_init
+  use pio,      only : iosystem_desc_t
 
   implicit none
   private
+
+  public :: mpp_pio_init
 
   character(len=64)   :: pio_netcdf_format, pio_typename
   integer             :: pio_numiotasks, pio_rearranger, pio_root, pio_stride
   namelist /mpp_pio_nml/ pio_netcdf_format, pio_numiotasks, pio_rearranger, &
                          pio_root, pio_stride, pio_typename
 
+  type(iosystem_desc_t) :: pio_iosystem     ! The ParallelIO system set up by PIO_init
+  integer               :: pio_optbase = 1  ! Start index of I/O processors
+
   contains
 
   subroutine mpp_pio_init()
     integer :: unit_begin, unit_end, unit_nml, io_status, unit
     logical :: opened
+    integer :: pe, npes, localcomm
+    integer :: numAggregator = 0 !TODO
 
-    ! namelist
+    ! read mpp_pio_nml namelist
 #ifdef INTERNAL_FILE_NML
       read (input_nml_file, mpp_pio_nml, iostat=io_status)
 #else
@@ -86,6 +101,26 @@ module mpp_pio_mod
       read(unit_nml,mpp_pio_nml,iostat=io_status)
       close(unit_nml)
 #endif
+
+    pe = mpp_pe()
+    npes = mpp_npes()
+    localcomm = get_mpp_comm()
+
+    ! TODO
+    ! add checks regarding the consistency of npes, numiotasksm stride, etc.
+
+    !initialize PIO
+    call PIO_init(    &
+      pe,             & ! MPI rank
+      localcomm,      & ! MPI communicator
+      pio_numiotasks, & ! Number of iotasks
+      numAggregator,  & ! number of aggregators to use
+      pio_stride,     & ! stride
+      pio_rearranger, & ! form of rearrangement
+      pio_iosystem,   & ! The ParallelIO system set up by PIO_init
+      pio_optbase )     ! Start index of I/O processors (optional)
+
+    print *, "initialized PIO: ", localcomm
 
   end subroutine mpp_pio_init
 
