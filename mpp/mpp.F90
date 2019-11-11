6b95896be18aec6bf10a7b519657755be9176f2f
alperaltuntas <alperaltuntas@gmail.com>
2019-11-11 15:22:41 -0700

    correct use_libMPI

--- mpp/mpp.F90
+++ mpp/mpp.F90
@@ -246,7 +246,7 @@ private
   public :: mpp_broadcast, mpp_malloc, mpp_init, mpp_exit
   public :: mpp_gather, mpp_scatter, mpp_alltoall
   public :: mpp_type, mpp_byte, mpp_type_create, mpp_type_free
-#ifdef use_MPI
+#ifdef use_libMPI
   public :: get_mpp_comm
 #endif
 #ifdef use_MPI_GSM
